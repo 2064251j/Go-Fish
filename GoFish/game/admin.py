00fd0cd097264fa92e056cb6e491a2d3fd7a326d
Michael Byars <md.byars@ntlworld.com>
2015-03-20 12:50:20 +0000

    Basic admin.py implementation. May require additions.

--- GoFish/game/admin.py
+++ GoFish/game/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from game.models import Game, Player, Card, Hand, Pool
 
-# Register your models here.
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('gameLobbyID', 'numOfPlayers', 'score')
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('gameLobbyID', 'playerID', 'displayName')
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('cardID')
+
+class HandAdmin(admin.ModelAdmin):
+    list_display = ('playerID', 'cardID')
+
+class PoolAdmin(admin.ModelAdmin):
+    list_display = ('cardID')
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(Hand, HandAdmin)
+admin.site.register(Pool, PoolAdmin)
+
