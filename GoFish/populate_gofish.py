fce119e4484b35038c47e0fdcca3ea4a074f0c40
Michael Byars <md.byars@ntlworld.com>
2015-03-23 23:25:32 +0000

    Card Population Script Added

--- GoFish/populate_gofish.py
+++ GoFish/populate_gofish.py
@@ -4,4 +4,29 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoFish.settings')
 import django
 django.setup()
 
-from game.models import Game, Player, Card, Hand, Pool
+from game.models import Player, Card
+
+def populate():
+
+    id = 1          #Individual Card ID = 1-52
+    suit = 1        #Hearts = 1, Clubs = 2, Diamonds = 3, Spades = 4
+    rank = 1        #1-13 Ace=1, 2-10, Jack=11, Queen=12, King=13
+    while id < 53:
+        add_card(id, suit, rank)
+        id += 1
+        rank += 1
+        if id%13 == 1:
+            rank = 1        #Resets rank back to 1 for each new suit. (Every 14th card)
+            suit += 1       #Increments suit every 14th card.
+
+def add_card(cardID, suit, rank):
+    c = Card.objects.get_or_create(cardID=cardID, suit=suit, rank=rank)[0]
+    return c
+
+if __name__ == '__main__':
+    print "Starting GoFish population script..."
+    populate()
+
+
+
+
